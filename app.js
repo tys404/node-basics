6d19dae857a9e3cde4b8f57e7d9afd5efea0a392
Krzysztof Tys <krzysztof.tys@gmail.com>
2023-12-20 12:59:53 +0100

    express middleware basics

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 import http from 'http';
-import routes from './routes.js';
+import express from 'express';
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+
+    next();
+});
+
+app.use((req, res, next) => {
+    res.send('<h1>Welcome!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
